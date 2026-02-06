7e5e83f2ba404e2f8a43f81d2c203e6ecb052998
jhziel <barnedodonjaziel57@gmail.com>
2026-02-06 08:29:28 +0800

    done on auto generating Resident Id

--- app/Models/Resident.php
+++ app/Models/Resident.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Resident extends Model
 {
@@ -11,6 +12,8 @@ class Resident extends Model
     use HasFactory;
 
     protected $fillable = [
+        'resident_id',
+        'barangay_code',
         'first_name',
         'last_name',
         'middle_name',
@@ -27,5 +30,34 @@ class Resident extends Model
     public function house_hold()
     {
         return $this->belongsTo(Household::class);
+    }
+
+    protected static function booted()
+    {
+        static::creating(function ($resident) {
+
+            DB::transaction(function () use ($resident) {
+
+                $barangayCode = $resident->barangay_code ?? 'BG';
+
+                $counter = DB::table('resident_counters')
+                    ->where('barangay_code', $barangayCode)
+                    ->lockForUpdate()
+                    ->first();
+
+                if (!$counter) {
+                    throw new \Exception("Counter not found for {$barangayCode}");
+                }
+
+                $newNumber = $counter->last_number + 1;
+
+                DB::table('resident_counters')
+                    ->where('id', $counter->id)
+                    ->update(['last_number' => $newNumber]);
+
+                $resident->resident_id =
+                    $barangayCode . str_pad($newNumber, 5, '0', STR_PAD_LEFT);
+            });
+        });
     }
 }
